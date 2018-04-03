51b077d09a3a1504f818821f6bb284e09cbe945e
= <erikschra2461@hotmail.com>
2018-04-03 14:38:10 -0400

    Minor optimizations
    turned some fields into statics to reduce object creation overhead.
    Refactored a bunch of the IL manipulation code for increased clarity.

--- PatchScripts/EntityZombieCorpseDropPatch.cs
+++ PatchScripts/EntityZombieCorpseDropPatch.cs
@@ -14,34 +14,74 @@ public class EntityZombieCorpseDropPatch : IPatcherMod
     {
         SDX.Core.Logging.LogInfo("Linking corpse fix module...");
 
-        TypeDefinition entityZombie = gameModule.Types.First(type => type.Name.Equals("EntityZombie"));
-        MethodDefinition dropCorpseBlockMethod = entityZombie.Methods.First(method => method.Name.Equals("dropCorpseBlock"));
-
-        MethodDefinition baseDropCorpseBlockMethod = gameModule.Types.First(type => type.Name.Equals("EntityAlive")).Methods.First(method => method.Name.Equals("dropCorpseBlock"));
-
-        Instruction instruction = dropCorpseBlockMethod.Body.Instructions.First(inst => inst.OpCode.Equals(OpCodes.Call) && inst.Operand.Equals(baseDropCorpseBlockMethod));
-
-        TypeDefinition entity = gameModule.Types.First(type => type.Name.Equals("Entity"));
-        FieldDefinition positionField = entity.Fields.First(field => field.Name.Equals("position"));
-        FieldDefinition worldField = entity.Fields.First(field => field.Name.Equals("world"));
-
-        TypeDefinition corpsePositionUpdater = modModule.Types.First(type => type.Name.Equals("ZombieCorpsePositionUpdater"));
-        MethodReference getUpdatedPosition = gameModule.Import(corpsePositionUpdater.Methods.First(method => method.Name.Equals("GetUpdatedPosition")));
+        MethodDefinition dropCorpseBlockMethod = GetCorpseDropMethodToAlter(gameModule);
+        Instruction callToBaseCorpseDropMethod = FindCallToBaseCorpseDropMethod(gameModule, dropCorpseBlockMethod);
 
         ILProcessor processor = dropCorpseBlockMethod.Body.GetILProcessor();
 
-        Instruction previousInstruction = Instruction.Create(instruction.Previous.OpCode);
-        previousInstruction.Operand = instruction.Previous.Operand;
+        Instruction copyOfExistingInstruction = OverwriteAndReAddInstruction(processor, callToBaseCorpseDropMethod.Previous, Instruction.Create(OpCodes.Ldarg_0));
 
-        instruction.Previous.OpCode = OpCodes.Ldarg_0;
-        processor.InsertAfter(instruction.Previous, previousInstruction);
-        
-        processor.InsertBefore(previousInstruction, Instruction.Create(OpCodes.Ldarg_0));
-        processor.InsertBefore(previousInstruction, Instruction.Create(OpCodes.Ldfld, worldField));
-        processor.InsertBefore(previousInstruction, Instruction.Create(OpCodes.Ldarg_0));
-        processor.InsertBefore(previousInstruction, Instruction.Create(OpCodes.Ldfld, positionField));
-        processor.InsertBefore(previousInstruction, Instruction.Create(OpCodes.Call, getUpdatedPosition));
-        processor.InsertBefore(previousInstruction, Instruction.Create(OpCodes.Stfld, positionField));
+        AddRemainingInstructionsBefore(processor, copyOfExistingInstruction, gameModule, modModule);
+
         return true;
+    }
+
+    private MethodDefinition GetCorpseDropMethodToAlter(ModuleDefinition gameModule)
+    {
+        TypeDefinition entityZombie = gameModule.Types.First(type => type.Name.Equals("EntityZombie"));
+        return entityZombie.Methods.First(method => method.Name.Equals("dropCorpseBlock"));
+    }
+
+    private Instruction FindCallToBaseCorpseDropMethod(ModuleDefinition gameModule, MethodDefinition corpseDropMethodToAlter)
+    {
+        MethodDefinition baseDropCorpseBlockMethod = gameModule.Types.First(type => type.Name.Equals("EntityAlive")).Methods.First(method => method.Name.Equals("dropCorpseBlock"));
+
+        return corpseDropMethodToAlter.Body.Instructions.First(inst => inst.OpCode.Equals(OpCodes.Call) && inst.Operand.Equals(baseDropCorpseBlockMethod));
+    }
+
+    private FieldDefinition GetPositionField(ModuleDefinition gameModule)
+    {
+        TypeDefinition entity = gameModule.Types.First(type => type.Name.Equals("Entity"));
+        return entity.Fields.First(field => field.Name.Equals("position"));
+    }
+
+    private MethodReference ImportCorpsePositionUpdaterIntoGameModule(ModuleDefinition gameModule, ModuleDefinition modModule)
+    {
+        TypeDefinition corpsePositionUpdater = modModule.Types.First(type => type.Name.Equals("ZombieCorpsePositionUpdater"));
+        return gameModule.Import(corpsePositionUpdater.Methods.First(method => method.Name.Equals("GetUpdatedPosition")));
+    }
+
+    private Instruction CreateCopyOfInstruction(Instruction instruction)
+    {
+        Instruction copyOfExistinInstruction = Instruction.Create(instruction.OpCode);
+        copyOfExistinInstruction.Operand = instruction.Operand;
+
+        return copyOfExistinInstruction;
+    }
+
+    private void ReplaceInstruction(Instruction existingInstruction, Instruction newInstruction)
+    {
+        existingInstruction.OpCode = newInstruction.OpCode;
+        existingInstruction.Operand = newInstruction.Operand;
+    }
+
+    private Instruction OverwriteAndReAddInstruction(ILProcessor processor, Instruction instructionToReplace, Instruction newInstruction)
+    {
+        Instruction copyOfExistingInstruction = CreateCopyOfInstruction(instructionToReplace);
+        ReplaceInstruction(instructionToReplace, newInstruction);
+        processor.InsertAfter(instructionToReplace, copyOfExistingInstruction);
+
+        return copyOfExistingInstruction;
+    }
+
+    private void AddRemainingInstructionsBefore(ILProcessor processor, Instruction instruction, ModuleDefinition gameModule, ModuleDefinition modModule)
+    {
+        MethodReference getUpdatedPosition = ImportCorpsePositionUpdaterIntoGameModule(gameModule, modModule);
+        FieldDefinition positionField = GetPositionField(gameModule);
+
+        processor.InsertBefore(instruction, Instruction.Create(OpCodes.Ldarg_0));
+        processor.InsertBefore(instruction, Instruction.Create(OpCodes.Ldfld, positionField));
+        processor.InsertBefore(instruction, Instruction.Create(OpCodes.Call, getUpdatedPosition));
+        processor.InsertBefore(instruction, Instruction.Create(OpCodes.Stfld, positionField));
     }
 }
--- Scripts/ZombieCorpsePositionUpdater.cs
+++ Scripts/ZombieCorpsePositionUpdater.cs
@@ -7,10 +7,11 @@ public static class ZombieCorpsePositionUpdater
     private const int MIN_HEIGHT = 3;
     private static readonly Configuration CONFIG = new Configuration(MAX_HEIGHT, MIN_HEIGHT, MAX_SEARCH_RADIUS);
 
-    public static Vector3 GetUpdatedPosition(World world, Vector3 position)
+    private static ZombieCorpsePositioner.GetBlock blockFinder = location => new BlockWrapper(GameManager.Instance.World.GetBlock(location).Block);
+    private static ZombieCorpsePositioner positioner = new ZombieCorpsePositioner(Debug.Log, blockFinder, new GroundFinder(CONFIG, blockFinder), CONFIG);
+
+    public static Vector3 GetUpdatedPosition(Vector3 position)
     {
-        ZombieCorpsePositioner.GetBlock blockFinder = location => new BlockWrapper(world.GetBlock(location).Block);
-        ZombieCorpsePositioner positioner = new ZombieCorpsePositioner(Debug.Log, blockFinder, new GroundFinder(CONFIG, blockFinder), CONFIG);
         Vector3i newPosition = positioner.FindSpawnLocationStartingFrom(World.worldToBlockPos(position));
         return new Vector3(newPosition.x, newPosition.y, newPosition.z);
     }
