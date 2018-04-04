3cada63411b42188581311fa6dead245021ca298
= <erikschra2461@hotmail.com>
2018-04-04 14:50:37 -0400

    Split HeightRange into dedicated class add tests
    Switched HeightRange from a struct into a class, and gave it a separate
    class file.
    
    Added unit tests and parameter validatio to HeightRange class.

---
+++ Scripts/HeightRange.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class HeightRange
+{
+    private int min;
+    private int max;
+    private int groundHeight;
+
+    public HeightRange(int min, int max, int groundHeight)
+    {
+        if (max < min)
+            throw new ArgumentException("The given min and max values are in the wrong order, given: " + min + "," + max, "min, max");
+        this.min = min;
+        this.max = max;
+        this.groundHeight = groundHeight;
+        if (!IsHeightInRange(groundHeight))
+            throw new ArgumentException("The given ground height is not within the given area, min:" + min + ", max:" + max + ", ground:" + groundHeight, "groundHeight");
+    }
+
+    public int Max
+    {
+        get { return max; }
+        set
+        {
+            if (value < min)
+                throw new ArgumentException("The new max value can not be less than the current min value, min: " + min + ", new max: " + value, "Max");
+            if (value < groundHeight)
+                throw new ArgumentException("The new max value can not be less than the current ground height, ground: " + groundHeight + ", new max: " + value, "Max");
+            max = value;
+        }
+    }
+
+    public int Min
+    {
+        get { return min; }
+        set
+        {
+            if (max < value)
+                throw new ArgumentException("The new min value can not be greater than the current max value, max: " + max + ", new min: " + value, "Min");
+            if (groundHeight < value)
+                throw new ArgumentException("The new min value can not be greater than the current ground height, ground: " + groundHeight + ", new min: " + value, "Min");
+            min = value;
+        }
+    }
+
+    public int GroundHeight
+    {
+        get { return groundHeight; }
+        set
+        {
+            if (value < min || max < value)
+                throw new ArgumentException("The new ground height can not be outside of the current height range, min: " + min + ", max: " + max + ", new ground: " + value, "GroundHeight");
+            groundHeight = value;
+        }
+    }
+
+    public bool IsHeightInRange(int height)
+    {
+        return min <= height && height <= max;
+    }
+
+    public bool IsOverlappingWith(HeightRange other)
+    {
+        return IsHeightInRange(other.min) || IsHeightInRange(other.max) || other.IsHeightInRange(min) || other.IsHeightInRange(max);
+    }
+}
---
+++ Unit Tests/HeightRangeTest.cs
@@ -0,0 +1,191 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class HeightRangeTest
+{
+    [Test]
+    public void WhenValuesAreSetInConstructorThenTheyCanBeRetrieved()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+
+        Assert.That(heightRange.Min, Is.EqualTo(2));
+        Assert.That(heightRange.Max, Is.EqualTo(5));
+        Assert.That(heightRange.GroundHeight, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void WhenMinAndMaxAreReversedThenExceptionIsThrown()
+    {
+        Assert.That(() => new HeightRange(5, 2, 3), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenGroundHeightIsOutsideOfMinAndMaxThenExceptionIsThrown()
+    {
+        Assert.That(() => new HeightRange(5, 2, 6), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenMinIsChangedToBeGreaterThanMaxThenExceptionIsThrown()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+        Assert.That(() => heightRange.Min = 6, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenMaxIsChangedToBeSmallerThanMinThenExceptionIsThrown()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+        Assert.That(() => heightRange.Max = 1, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenMinIsChangedToExcludeGroundHeightThenExceptionIsThrown()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+        Assert.That(() => heightRange.Min = 4, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenMaxIsChangedToExcludeGroundHeightThenExceptionIsThrown()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+        Assert.That(() => heightRange.Max = 2, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenGroundHeightIsChangedToBeLowerThanMinThenExceptionIsThrown()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+        Assert.That(() => heightRange.GroundHeight = 1, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WhenGroundHeightIsChangedToBeHigherThanMaxThenExceptionIsThrown()
+    {
+        HeightRange heightRange = new HeightRange(2, 5, 3);
+        Assert.That(() => heightRange.GroundHeight = 6, Throws.ArgumentException);
+    }
+
+    [Test]
+    public void HeightRangeCanBeOneUnitLong()
+    {
+        HeightRange heightRange = new HeightRange(2, 2, 2);
+
+        Assert.That(heightRange.Min, Is.EqualTo(2));
+        Assert.That(heightRange.Max, Is.EqualTo(2));
+        Assert.That(heightRange.GroundHeight, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void WhenHeightIsLessThanMinThenItIsNotInRange()
+    {
+        HeightRange heightRange = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRange.IsHeightInRange(1), Is.False);
+    }
+
+    [Test]
+    public void WhenHeightIsGreaterThanMaxThenItIsNotInRange()
+    {
+        HeightRange heightRange = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRange.IsHeightInRange(10), Is.False);
+    }
+
+    [Test]
+    public void WhenHeightIsEqualToMinThenItIsInRange()
+    {
+        HeightRange heightRange = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRange.IsHeightInRange(2), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightIsEqualToMaxThenItIsInRange()
+    {
+        HeightRange heightRange = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRange.IsHeightInRange(8), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightIsInBetweenMinAndMaxThenItIsInRange()
+    {
+        HeightRange heightRange = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRange.IsHeightInRange(4), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeHasMinInBetweenMinAndMaxOfOtherHeightRangeThenTheyAreOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(6, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRangeOne.IsOverlappingWith(heightRangeTwo), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeHasMaxInBetweenMinAndMaxOfOtherHeightRangeThenTheyAreOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(6, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 8, 5);
+
+        Assert.That(heightRangeTwo.IsOverlappingWith(heightRangeOne), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeHasMinEqualToMaxOfOtherHeightRangeThenTheyAreOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(6, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 6, 5);
+
+        Assert.That(heightRangeOne.IsOverlappingWith(heightRangeTwo), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeHasMaxEqualToMinOfOtherHeightRangeThenTheyAreOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(6, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 6, 5);
+
+        Assert.That(heightRangeTwo.IsOverlappingWith(heightRangeOne), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeEncompasesAnotherHeightRangeThenTheyAreOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(1, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 6, 5);
+
+        Assert.That(heightRangeOne.IsOverlappingWith(heightRangeTwo), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeIsEncompassedByAnotherHeightRangeThenTheyAreOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(1, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 6, 5);
+
+        Assert.That(heightRangeTwo.IsOverlappingWith(heightRangeOne), Is.True);
+    }
+
+    [Test]
+    public void WhenHeightRangeIsHigherThanAnotherHeightRangeThenTheyAreNotOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(10, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 6, 5);
+
+        Assert.That(heightRangeOne.IsOverlappingWith(heightRangeTwo), Is.False);
+    }
+
+    [Test]
+    public void WhenHeightRangeIsLowerThanAnotherHeightRangeThenTheyAreNotOverlapping()
+    {
+        HeightRange heightRangeOne = new HeightRange(10, 20, 15);
+        HeightRange heightRangeTwo = new HeightRange(2, 6, 5);
+
+        Assert.That(heightRangeTwo.IsOverlappingWith(heightRangeOne), Is.False);
+    }
+}
